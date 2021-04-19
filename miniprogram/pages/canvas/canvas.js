e85a391279a47dcec0eca68cd808e164e17f576c
unknown <unknown@example.com>
2021-04-19 15:12:28 +0800

    1

--- miniprogram/pages/canvas/canvas.js
+++ miniprogram/pages/canvas/canvas.js
@@ -5,9 +5,109 @@ Page({
    * 页面的初始数据
    */
   data: {
-       array:[1,2,3,4,5]
+
   },
 onLoad(){
-  console.log(undefined==null)
-}
+  var that = this;
+    wx.showLoading({
+   
+    title: '开启蓝牙适配'
+   
+    });
+   
+    wx.openBluetoothAdapter({
+   
+    success: function (res) {
+   
+    console.log("初始化蓝牙适配器");
+   
+    console.log(res);
+   
+    that.getBluetoothAdapterState();
+   
+    },
+   
+    fail: function (err) {
+   
+    console.log(err);
+    
+    wx.showToast({
+   
+    title: '蓝牙初始化失败',
+   
+    icon: 'success',
+   
+    duration: 2000
+   
+    })
+   
+    setTimeout(function () {
+   
+    wx.hideToast()
+   
+    }, 2000)
+   
+    }
+   
+    });
+   
+    wx.onBluetoothAdapterStateChange(function (res) {
+   
+    var available = res.available;
+   
+    if (available) {
+   
+    that.getBluetoothAdapterState();
+   
+    }
+   
+    })
+},
+getBluetoothAdapterState: function () {
+
+  var that = this;
+ 
+  wx.getBluetoothAdapterState({
+ 
+  success: function (res) {
+ 
+  var available = res.available,
+ 
+  discovering = res.discovering;
+ 
+  if (!available) {
+ 
+  wx.showToast({
+ 
+  title: '设备无法开启蓝牙连接',
+ 
+  icon: 'success',
+ 
+  duration: 2000
+ 
+  })
+ 
+  setTimeout(function () {
+ 
+  wx.hideToast()
+ 
+  }, 2000)
+ 
+  } else {
+ 
+  if (!discovering) {
+ 
+  that.startBluetoothDevicesDiscovery();
+ 
+  that.getConnectedBluetoothDevices();
+ 
+  }
+ 
+  }
+ 
+  }
+ 
+  })
+ 
+  }
 })
